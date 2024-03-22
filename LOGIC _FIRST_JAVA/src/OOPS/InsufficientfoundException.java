23796b384852ed3fe8d394659a90256a370488a7
venkatasubaramanian97 <venkatasubaramanian9751@gmail.com>
2024-03-22 18:48:37 +0530

    delete injava

--- LOGIC _FIRST_JAVA/src/OOPS/InsufficientfoundException.java
+++
@@ -1,9 +0,0 @@
-package OOPS;
-
-@SuppressWarnings("serial")
-public class InsufficientfoundException extends Exception {
-	double amount;
-	InsufficientfoundException(double amt){
-		amount=amt;
-	}
-}
--- LOGIC_FIRST_DSA/src/BinaryDemo.java
+++
@@ -1,9 +0,0 @@
-
-public class BinaryDemo {
-
-	public static void main(String[] args) {
-int array[]= {100,95,93,84,80,76,70,8,1};
-System.out.println("Index of the value : "+BinarySearch.search(array,100));
-	}
-
-}
